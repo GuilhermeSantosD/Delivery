312ecfdeeeac2ef82afef5ff7831c39235df14a9
aspiranteDev <guilherme.bluesman@gmail.com>
2024-02-07 18:54:07 -0300

    pedido cliente

--- src/main/java/com/jpa/course/entites/User.java
+++ src/main/java/com/jpa/course/entites/User.java
@@ -1,11 +1,14 @@
 package com.jpa.course.entites;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -21,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	
 	public User() {
@@ -85,8 +91,14 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
+	
+		
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -112,7 +124,6 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 
 }
