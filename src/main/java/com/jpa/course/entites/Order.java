0d7b6070a0a3a731f713eb69fe52215e1b46c6e1
aspiranteDev <guilherme.bluesman@gmail.com>
2024-03-24 15:12:12 -0300

    implements payment

--- src/main/java/com/jpa/course/entites/Order.java
+++ src/main/java/com/jpa/course/entites/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jpa.course.entites.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -41,6 +43,8 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy= "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 
@@ -95,6 +99,14 @@ public class Order implements Serializable {
 	}
 	
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
