94cdf4ad6df2c878e63aa7568a92916f05daf1ac
aspiranteDev <guilherme.bluesman@gmail.com>
2024-03-26 21:11:24 -0300

    exception update

--- src/main/java/com/jpa/course/services/UserService.java
+++ src/main/java/com/jpa/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.jpa.course.repositories.UserRepository;
 import com.jpa.course.services.exceptions.DatabaseException;
 import com.jpa.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,14 @@ public class UserService {
 		}
 	
 	public User update (Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData (entity, obj);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e){
+			throw new  ResourceNotFoundException(id);
+	
+		}
 	}
 
 	private void updateData(User entity, User obj) {
